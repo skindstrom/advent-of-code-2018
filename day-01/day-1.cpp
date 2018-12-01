c46ab753f0af89511b4f8a3eb45489be9622bd2c
Simon Kindström <simon.kindstrom@gmail.com>
2018-12-01 12:05:39 +0100

    Day 1 - Part 2

--- day-01/day-1.cpp
+++ day-01/day-1.cpp
@@ -1,14 +1,21 @@
 #include <fstream>
 #include <iostream>
+#include <range/v3/algorithm/for_each.hpp>
 #include <range/v3/numeric/accumulate.hpp>
+#include <unordered_set>
 #include <vector>
 
 using frequency_update = int;
 
 std::vector<frequency_update> readFrequencyUpdates();
 int calculateFrequency(const std::vector<frequency_update> &updates);
+int findDuplicateFrequency(const std::vector<frequency_update> &updates);
 
-int main() { std::cout << calculateFrequency(readFrequencyUpdates()) << '\n'; }
+int main() {
+  const auto frequencyUpdates = readFrequencyUpdates();
+  std::cout << "Part 1: " << calculateFrequency(frequencyUpdates) << '\n';
+  std::cout << "Part 2: " << findDuplicateFrequency(frequencyUpdates) << '\n';
+}
 
 std::vector<frequency_update> readFrequencyUpdates() {
   std::vector<frequency_update> numbers;
@@ -24,4 +31,22 @@ std::vector<frequency_update> readFrequencyUpdates() {
 
 int calculateFrequency(const std::vector<frequency_update> &updates) {
   return ranges::accumulate(updates, 0);
+}
+
+int findDuplicateFrequency(const std::vector<frequency_update> &updates) {
+  std::unordered_set<int> frequencies;
+
+  int frequency = 0;
+  auto update = updates.cbegin();
+  while (frequencies.count(frequency) == 0) {
+    frequencies.emplace(frequency);
+    frequency += *update;
+
+    ++update;
+    if (update == updates.cend()) {
+      update = updates.cbegin();
+    }
+  }
+
+  return frequency;
 }
